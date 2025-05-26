c94cf61fc1dc4ce712ba997e589857141dd2c00b
Lincoln <lincolngabriel19@gmail.com>
2025-05-25 22:05:14 -0300

    fix: update endereco detalhado

--- src/main/java/br/com/fiap/tech_challenge_1/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/com/fiap/tech_challenge_1/service/impl/UsuarioServiceImpl.java
@@ -1,125 +1,132 @@
-package br.com.fiap.tech_challenge_1.service.impl;
+  package br.com.fiap.tech_challenge_1.service.impl;
 
-import br.com.fiap.tech_challenge_1.dto.request.UsuarioEditRequest;
-import br.com.fiap.tech_challenge_1.dto.request.UsuarioLoginRequest;
-import br.com.fiap.tech_challenge_1.dto.request.UsuarioRequest;
-import br.com.fiap.tech_challenge_1.dto.response.UsuarioResponse;
-import br.com.fiap.tech_challenge_1.exception.AuthenticationException;
-import br.com.fiap.tech_challenge_1.exception.DuplicateResourceException;
-import br.com.fiap.tech_challenge_1.exception.ResourceNotFoundException;
-import br.com.fiap.tech_challenge_1.mapper.EnderecoMapper;
-import br.com.fiap.tech_challenge_1.mapper.UsuarioMapper;
-import br.com.fiap.tech_challenge_1.model.Endereco;
-import br.com.fiap.tech_challenge_1.model.Usuario;
-import br.com.fiap.tech_challenge_1.repository.EnderecoRepository;
-import br.com.fiap.tech_challenge_1.repository.UsuarioRepository;
-import br.com.fiap.tech_challenge_1.service.UsuarioService;
-import br.com.fiap.tech_challenge_1.utils.PasswordHasher;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+  import br.com.fiap.tech_challenge_1.dto.request.UsuarioEditRequest;
+  import br.com.fiap.tech_challenge_1.dto.request.UsuarioLoginRequest;
+  import br.com.fiap.tech_challenge_1.dto.request.UsuarioRequest;
+  import br.com.fiap.tech_challenge_1.dto.response.UsuarioResponse;
+  import br.com.fiap.tech_challenge_1.exception.AuthenticationException;
+  import br.com.fiap.tech_challenge_1.exception.DuplicateResourceException;
+  import br.com.fiap.tech_challenge_1.exception.ResourceNotFoundException;
+  import br.com.fiap.tech_challenge_1.mapper.EnderecoMapper;
+  import br.com.fiap.tech_challenge_1.mapper.UsuarioMapper;
+  import br.com.fiap.tech_challenge_1.model.Endereco;
+  import br.com.fiap.tech_challenge_1.model.Usuario;
+  import br.com.fiap.tech_challenge_1.repository.EnderecoRepository;
+  import br.com.fiap.tech_challenge_1.repository.UsuarioRepository;
+  import br.com.fiap.tech_challenge_1.service.UsuarioService;
+  import br.com.fiap.tech_challenge_1.utils.PasswordHasher;
+  import lombok.RequiredArgsConstructor;
+  import org.springframework.stereotype.Service;
+  import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+  import java.util.Set;
+  import java.util.stream.Collectors;
 
-@Service
-@RequiredArgsConstructor
-public class UsuarioServiceImpl implements UsuarioService {
+  @Service
+  @RequiredArgsConstructor
+  public class UsuarioServiceImpl implements UsuarioService {
 
-  private final UsuarioRepository usuarioRepository;
-  private final PasswordHasher passwordHasher;
-  private final UsuarioMapper usuarioMapper;
-  private final EnderecoMapper enderecoMapper;
-  private final EnderecoRepository enderecoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordHasher passwordHasher;
+    private final UsuarioMapper usuarioMapper;
+    private final EnderecoMapper enderecoMapper;
+    private final EnderecoRepository enderecoRepository;
 
-  @Override
-  @Transactional
-  public UsuarioResponse save(UsuarioRequest request) {
+    @Override
+    @Transactional
+    public UsuarioResponse save(UsuarioRequest request) {
 
-    usuarioRepository
-        .findByLogin(request.login())
-        .ifPresent(
-            u -> {
-              throw new DuplicateResourceException("Login já está em uso");
-            });
+      usuarioRepository
+          .findByLogin(request.login())
+          .ifPresent(
+              u -> {
+                throw new DuplicateResourceException("Login já está em uso");
+              });
 
-    Usuario usuario = usuarioMapper.toEntity(request);
-    usuario.setSenha(passwordHasher.hashPassword(request.senha()));
+      Usuario usuario = usuarioMapper.toEntity(request);
+      usuario.setSenha(passwordHasher.hashPassword(request.senha()));
 
-    Usuario saved = usuarioRepository.save(usuario);
-    return usuarioMapper.toResponse(saved);
-  }
-
-  @Override
-  @Transactional(readOnly = true)
-  public Set<UsuarioResponse> findAll() {
-    return usuarioRepository.findAll().stream()
-        .map(usuarioMapper::toResponse)
-        .collect(Collectors.toSet());
-  }
-
-  @Override
-  @Transactional(readOnly = true)
-  public UsuarioResponse findById(Long id) {
-    return usuarioRepository
-        .findById(id)
-        .map(usuarioMapper::toResponse)
-        .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com id: " + id));
-  }
-
-  @Override
-  @Transactional(readOnly = true)
-  public boolean authenticate(UsuarioLoginRequest loginRequest) {
-    Usuario usuario =
-        usuarioRepository
-            .findByLogin(loginRequest.login())
-            .orElseThrow(() -> new AuthenticationException("Login não encontrado"));
-
-    boolean authenticated = passwordHasher.verifyPassword(loginRequest.senha(), usuario.getSenha());
-    if (!authenticated) {
-      throw new AuthenticationException("Senha incorreta");
-    }
-    return true;
-  }
-
-  @Override
-  @Transactional
-  public UsuarioResponse update(Long id, UsuarioEditRequest request) {
-    Usuario existingUsuario =
-        usuarioRepository
-            .findById(id)
-            .orElseThrow(
-                () -> new ResourceNotFoundException("Usuário não encontrado com id: " + id));
-
-
-    Endereco endereco = null;
-    if (request.endereco() != null) {
-      endereco = enderecoMapper.toEndereco(request.endereco());
-      endereco = enderecoRepository.save(endereco);
+      Usuario saved = usuarioRepository.save(usuario);
+      return usuarioMapper.toResponse(saved);
     }
 
-    if (endereco != null) {
-      existingUsuario.setEndereco(endereco);
+    @Override
+    @Transactional(readOnly = true)
+    public Set<UsuarioResponse> findAll() {
+      return usuarioRepository.findAll().stream()
+          .map(usuarioMapper::toResponse)
+          .collect(Collectors.toSet());
     }
 
-    existingUsuario.setNome((request.nome() != null && !request.nome().isBlank()) ? request.nome() : existingUsuario.getNome());
-    existingUsuario.setEmail((request.email() != null && !request.email().isBlank()) ? request.email() : existingUsuario.getEmail());
-    existingUsuario.setPerfil((request.perfil() != null && !request.perfil().name().isBlank()) ? request.perfil().name() : existingUsuario.getPerfil());
-
-    if (request.senha() != null && !request.senha().isEmpty()) {
-      existingUsuario.setSenha(passwordHasher.hashPassword(request.senha()));
+    @Override
+    @Transactional(readOnly = true)
+    public UsuarioResponse findById(Long id) {
+      return usuarioRepository
+          .findById(id)
+          .map(usuarioMapper::toResponse)
+          .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com id: " + id));
     }
 
-    Usuario updated = usuarioRepository.save(existingUsuario);
-    return usuarioMapper.toResponse(updated);
+    @Override
+    @Transactional(readOnly = true)
+    public boolean authenticate(UsuarioLoginRequest loginRequest) {
+      Usuario usuario =
+          usuarioRepository
+              .findByLogin(loginRequest.login())
+              .orElseThrow(() -> new AuthenticationException("Login não encontrado"));
+
+      boolean authenticated = passwordHasher.verifyPassword(loginRequest.senha(), usuario.getSenha());
+      if (!authenticated) {
+        throw new AuthenticationException("Senha incorreta");
+      }
+      return true;
+    }
+
+    @Override
+    @Transactional
+    public UsuarioResponse update(Long id, UsuarioEditRequest request) {
+      Usuario existingUsuario =
+          usuarioRepository
+              .findById(id)
+              .orElseThrow(
+                  () -> new ResourceNotFoundException("Usuário não encontrado com id: " + id));
+
+      if (request.endereco() != null) {
+        updateEndereco(request, existingUsuario);
+      }
+
+      updateUsuario(request, existingUsuario);
+
+      Usuario updated = usuarioRepository.save(existingUsuario);
+      return usuarioMapper.toResponse(updated);
+    }
+
+    private void updateUsuario(UsuarioEditRequest request, Usuario existingUsuario) {
+      existingUsuario.setNome((request.nome() != null && !request.nome().isBlank()) ? request.nome() : existingUsuario.getNome());
+      existingUsuario.setEmail((request.email() != null && !request.email().isBlank()) ? request.email() : existingUsuario.getEmail());
+      existingUsuario.setPerfil((request.perfil() != null && !request.perfil().name().isBlank()) ? request.perfil().name() : existingUsuario.getPerfil());
+
+      if (request.senha() != null && !request.senha().isEmpty()) {
+        existingUsuario.setSenha(passwordHasher.hashPassword(request.senha()));
+      }
+    }
+
+    private void updateEndereco(UsuarioEditRequest request, Usuario existingUsuario) {
+     existingUsuario.getEndereco().setBairro((request.endereco().bairro() != null && !request.endereco().bairro().isBlank() ? request.endereco().bairro() : existingUsuario.getEndereco().getBairro()));
+      existingUsuario.getEndereco().setCidade((request.endereco().cidade() != null && !request.endereco().cidade().isBlank() ? request.endereco().cidade() : existingUsuario.getEndereco().getCidade()));
+      existingUsuario.getEndereco().setLogradouro((request.endereco().logradouro() != null && !request.endereco().logradouro().isBlank() ? request.endereco().logradouro() : existingUsuario.getEndereco().getLogradouro()));
+      existingUsuario.getEndereco().setCep((request.endereco().cep() != null && !request.endereco().cep().isBlank() ? request.endereco().cep() : existingUsuario.getEndereco().getCep()));
+      existingUsuario.getEndereco().setComplemento((request.endereco().complemento() != null && !request.endereco().complemento().isBlank() ? request.endereco().complemento() : existingUsuario.getEndereco().getComplemento()));
+      existingUsuario.getEndereco().setEstado((request.endereco().estado() != null && !request.endereco().estado().isBlank() ? request.endereco().estado() : existingUsuario.getEndereco().getEstado()));
+      existingUsuario.getEndereco().setNumero((request.endereco().numero() != null && !request.endereco().numero().isBlank() ? request.endereco().numero() : existingUsuario.getEndereco().getNumero()));
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+      if (!usuarioRepository.existsById(id)) {
+        throw new ResourceNotFoundException("Usuário não encontrado com id: " + id);
+      }
+      usuarioRepository.deleteById(id);
+    }
   }
-
-  @Override
-  @Transactional
-  public void delete(Long id) {
-    if (!usuarioRepository.existsById(id)) {
-      throw new ResourceNotFoundException("Usuário não encontrado com id: " + id);
-    }
-    usuarioRepository.deleteById(id);
-  }
-}
